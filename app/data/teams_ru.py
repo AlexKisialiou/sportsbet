12824053980fefb31b014906a0bdb9573fd08da3
Aliaksei_Kisialeu <aliaksei.kisiaaliou@gmail.com>
2026-04-16 09:48:33 +0300

    Add TEAMS_RU dictionary, apply Russian names when saving teams from API

---
+++ app/data/teams_ru.py
@@ -0,0 +1,84 @@
+# Russian translations for team names
+# Key: English name from football-data.org API
+# Value: Russian name
+TEAMS_RU = {
+    # Испания
+    "Club Atlético de Madrid"           : "Атлетико Мадрид",
+    "Real Madrid CF"                    : "Реал Мадрид",
+    "FC Barcelona"                      : "Барселона",
+    "Sevilla FC"                        : "Севилья",
+    "Real Sociedad de Fútbol"           : "Реал Сосьедад",
+    "Villarreal CF"                     : "Вильярреал",
+    "Athletic Club"                     : "Атлетик Бильбао",
+
+    # Англия
+    "Manchester City FC"                : "Манчестер Сити",
+    "Liverpool FC"                      : "Ливерпуль",
+    "Chelsea FC"                        : "Челси",
+    "Arsenal FC"                        : "Арсенал",
+    "Manchester United FC"              : "Манчестер Юнайтед",
+    "Tottenham Hotspur FC"              : "Тоттенхэм",
+    "Newcastle United FC"               : "Ньюкасл",
+
+    # Германия
+    "FC Bayern München"                 : "Бавария",
+    "Borussia Dortmund"                 : "Боруссия Дортмунд",
+    "Bayer 04 Leverkusen"               : "Байер Леверкузен",
+    "RB Leipzig"                        : "РБ Лейпциг",
+    "VfB Stuttgart"                     : "Штутгарт",
+    "Eintracht Frankfurt"               : "Айнтрахт Франкфурт",
+
+    # Италия
+    "FC Internazionale Milano"          : "Интер",
+    "AC Milan"                          : "Милан",
+    "Juventus FC"                       : "Ювентус",
+    "AS Roma"                           : "Рома",
+    "SSC Napoli"                        : "Наполи",
+    "ACF Fiorentina"                    : "Фиорентина",
+    "Atalanta BC"                       : "Аталанта",
+
+    # Франция
+    "Paris Saint-Germain FC"            : "ПСЖ",
+    "Olympique de Marseille"            : "Марсель",
+    "Olympique Lyonnais"                : "Лион",
+    "AS Monaco FC"                      : "Монако",
+    "LOSC Lille"                        : "Лилль",
+    "Stade Brestois 29"                 : "Брест",
+
+    # Португалия
+    "Sport Lisboa e Benfica"            : "Бенфика",
+    "FC Porto"                          : "Порту",
+    "Sporting CP"                       : "Спортинг",
+
+    # Нидерланды
+    "AFC Ajax"                          : "Аякс",
+    "PSV Eindhoven"                     : "ПСВ",
+    "Feyenoord Rotterdam"               : "Фейеноорд",
+
+    # Бельгия
+    "Club Brugge KV"                    : "Брюгге",
+    "RSC Anderlecht"                    : "Андерлехт",
+
+    # Австрия
+    "FC Red Bull Salzburg"              : "Зальцбург",
+    "SK Sturm Graz"                     : "Штурм Грац",
+
+    # Шотландия
+    "Celtic FC"                         : "Селтик",
+    "Rangers FC"                        : "Рейнджерс",
+
+    # Чехия
+    "AC Sparta Praha"                   : "Спарта Прага",
+
+    # Хорватия
+    "GNK Dinamo Zagreb"                 : "Динамо Загреб",
+
+    # Сербия
+    "FK Crvena zvezda"                  : "Црвена Звезда",
+
+    # Швейцария
+    "BSC Young Boys"                    : "Янг Бойз",
+
+    # Дания
+    "FC Copenhagen"                     : "Копенгаген",
+}
